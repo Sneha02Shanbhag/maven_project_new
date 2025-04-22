b49cd26fc42a5e0849b03f6a016bddc4505d097d
Sneha Shanbhag <159918012+Sneha02Shanbhag@users.noreply.github.com>
2025-04-22 15:35:23 +0530

    Add files via upload

---
+++ firstProgram/demoTest/src/main/java/com/sneha/demoTest/HelloWorld.java
@@ -0,0 +1,10 @@
+package com.sneha.demoTest;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+@RestController
+public class HelloWorld {
+	@GetMapping("/")
+	public String sayHello() {
+	return "Hello, World";
+	}
+}
---
+++ firstProgram/demoTest/src/test/java/com/sneha/demoTest/DemoTestApplicationTests.java
@@ -0,0 +1,13 @@
+package com.sneha.demoTest;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DemoTestApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
